7dafa10f0572c10c4daeb020e010d788381d9f0a
Aluno em Vocação Yasmin <yasilvalves26@gmail.com>
2026-08-18 16:42:05 -0300

    update

---
+++ modulo08_meudesenho_yasmin.py
@@ -0,0 +1,27 @@
+'''
+DPA - Berenice
+Fronze - Elsa
+My little pony - Rarity
+'''
+
+
+
+class clubinhoDPA
+   def __init__(self, nome, dono, frase_de_efeito):
+      self.personagens =
+      self.nome = nome
+      self.dono = dono
+      self.frase_de_efeito = frase_de_efeito
+
+
+
+
+woody = BonecoToyStory()
+nome = "woody",
+ dono = "andy" ,
+frase_de_efeito = "Ha uma cobra na minha bota!"  
+
+betty = BonecoToyStory
+nome = betty =
+dono = molly 
+frase_de_efeito = "quem disse que as meninas não pode se divertir"
